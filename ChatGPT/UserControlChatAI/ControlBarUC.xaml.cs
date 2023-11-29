acf26398bab1e95c6721b75ef1f4747845a40dcd
Bich Son Nhat <55039261+bichsonnhat@users.noreply.github.com>
2023-11-29 17:09:22 +0700

    Add files via upload

---
+++ ChatGPT/UserControlChatAI/ControlBarUC.xaml.cs
@@ -0,0 +1,55 @@
+﻿using ChatAI.ViewModel.ControlBarVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatAI.UserControlChatAI
+{
+    /// <summary>
+    /// Interaction logic for ControlBarUC.xaml
+    /// </summary>
+    public partial class ControlBarUC : UserControl
+    {
+        public ControlBarViewModel ViewModel { get; set; }
+        public ControlBarUC()
+        {
+            InitializeComponent();
+            this.DataContext = ViewModel = new ControlBarViewModel();
+        }
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Button btn = sender as Button;
+
+            btn.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#595757");
+        }
+        private void Button_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Button btn = sender as Button;
+
+            btn.Background = new SolidColorBrush(Colors.Transparent);
+        }
+        private void Button_MouseEnter_1(object sender, MouseEventArgs e)
+        {
+            Button btn = sender as Button;
+
+            btn.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#ff0000");
+        }
+        private void Button_MouseLeave_1(object sender, MouseEventArgs e)
+        {
+            Button btn = sender as Button;
+            btn.Background = new SolidColorBrush(Colors.Transparent);
+        }
+    }
+}
