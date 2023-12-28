317935f262460349b207db9ea2b53db0d243399d
Bich Son Nhat <bichsonnhat@gmail.com>
2023-12-29 01:29:21 +0700

    fix error

--- ChatGPT/View/WordWindow.xaml.cs
+++ ChatGPT/View/WordWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace ChatWpfUI.View
+namespace ChatAI.View
 {
     /// <summary>
     /// Interaction logic for WordWindow.xaml
